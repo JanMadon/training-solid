f5f874867f92c0f752f11b54be4ef272901492af
Jan <jan.madon@gmail.com>
2025-02-02 11:43:50 +0100

    added Course ValueObject used in CoursesRepository, implemented in tests and view

--- app/Repositories/Db/CoursesRepositoryDb.php
+++ app/Repositories/Db/CoursesRepositoryDb.php
@@ -3,6 +3,9 @@
 namespace App\Repositories\Db;
 
 use App\Repositories\CourseRepository;
+use App\ValueObjects\Course;
+use Illuminate\Database\Query\Builder;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
 use DB;
 
@@ -11,33 +14,59 @@ class CoursesRepositoryDb implements CourseRepository
 
     public function getLatestCourses(int $limit): Collection
     {
-        return DB::table('courses')
-            ->select('courses.*', 'instructors.first_name', 'instructors.last_name')
-            ->selectRaw('AVG(course_ratings.rating) AS average_rating')
-            ->leftJoin('course_ratings', 'course_ratings.course_id', '=', 'courses.id')
-            ->join('instructors', 'instructors.id', '=', 'courses.instructor_id')
-            ->where('courses.is_active', true)
-            ->groupBy('courses.id')
-            ->limit($limit)
+        $courses = $this->queryBuilder($limit)
             ->orderBy('courses.updated_at', 'desc')
             ->get();
+
+        return $this->getValueObject($courses);
     }
 
     public function getFreeCourses(int $limit): Collection
     {
-        return DB::table('courses')
-            ->select('courses.*', 'instructors.first_name', 'instructors.last_name')
-            ->selectRaw('AVG(course_ratings.rating) AS average_rating')
-            ->leftJoin('course_ratings', 'course_ratings.course_id', '=', 'courses.id')
-            ->join('instructors', 'instructors.id', '=', 'courses.instructor_id')
-            ->where('courses.is_active', true)
-            ->groupBy('courses.id')
-            ->limit($limit)
+        $courses = $this->queryBuilder($limit)
             ->where('courses.price', 0)
             ->get();
+
+        return $this->getValueObject($courses);
     }
 
     public function getDiscountCourses(int $limit): Collection
+    {
+        $courses = $this->queryBuilder($limit)
+            ->where('courses.strike_out_price', '<>' ,null)
+            ->get();
+
+        return $this->getValueObject($courses);
+
+    }
+
+
+    private function getValueObject(Collection $courses): collection
+    {
+        return $courses->map(fn($course) => new Course(
+            $course->id,
+            $course->first_name,
+            $course->last_name,
+            $course->instructor_id,
+            $course->category_id,
+            $course->instruction_level_id,
+            $course->course_title,
+            $course->course_slug,
+            $course->keywords,
+            $course->overview,
+            $course->course_image,
+            $course->thumb_image,
+            $course->course_video,
+            $course->duration,
+            $course->price,
+            $course->strike_out_price,
+            $course->is_active,
+            Carbon::make($course->created_at),
+            Carbon::make($course->updated_at),
+        ));
+    }
+
+    private function queryBuilder(int $limit): Builder
     {
         return DB::table('courses')
             ->select('courses.*', 'instructors.first_name', 'instructors.last_name')
@@ -46,8 +75,6 @@ class CoursesRepositoryDb implements CourseRepository
             ->join('instructors', 'instructors.id', '=', 'courses.instructor_id')
             ->where('courses.is_active', true)
             ->groupBy('courses.id')
-            ->limit($limit)
-            ->where('courses.strike_out_price', '<>' ,null)
-            ->get();
+            ->limit($limit);
     }
 }
--- tests/Feature/CoursesRepository.php
+++ tests/Feature/CoursesRepository.php
@@ -8,6 +8,7 @@ use App\Models\InstructionLevel;
 use App\Models\Instructor;
 use App\Models\User;
 use App\Repositories\Db\CoursesRepositoryDb;
+use App\ValueObjects\Course as CourseVO;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\Factories\CreateCourses;
@@ -35,38 +36,16 @@ class CoursesRepository extends TestCase
         $discounted = $repository->getDiscountCourses(1);
 
         // Then:
-        $this->assertEquals($course->getKey(), $latest->first()->id);
-        $this->assertEquals($course->getKey(), $free->first()->id);
-        $this->assertEquals($course->getKey(), $discounted->first()->id);
+        $this->assertEquals($course->getKey(), $latest->first()->getId());
+        $this->assertEquals($course->getKey(), $free->first()->getId());
+        $this->assertEquals($course->getKey(), $discounted->first()->getId());
 
-        $this->assertValidStructureOfCourse($latest->first());
-        $this->assertValidStructureOfCourse($free->first());
-        $this->assertValidStructureOfCourse($discounted->first());
+        $this->assertInstanceOf(CourseVO::class, $latest->first());
+        $this->assertInstanceOf(CourseVO::class, $free->first());
+        $this->assertInstanceOf(CourseVO::class, $discounted->first());
 
         $this->assertCount(1, $latest);
         $this->assertCount(1, $free);
         $this->assertCount(1, $discounted);
-    }
-    private function assertValidStructureOfCourse($latest): void
-    {
-        $this->assertArrayHasKey('id', (array)$latest);
-        $this->assertArrayHasKey('instructor_id', (array)$latest);
-        $this->assertArrayHasKey('category_id', (array)$latest);
-        $this->assertArrayHasKey('instruction_level_id', (array)$latest);
-        $this->assertArrayHasKey('course_title', (array)$latest);
-        $this->assertArrayHasKey('course_slug', (array)$latest);
-        $this->assertArrayHasKey('keywords', (array)$latest);
-        $this->assertArrayHasKey('overview', (array)$latest);
-        $this->assertArrayHasKey('course_image', (array)$latest);
-        $this->assertArrayHasKey('thumb_image', (array)$latest);
-        $this->assertArrayHasKey('course_video', (array)$latest);
-        $this->assertArrayHasKey('duration', (array)$latest);
-        $this->assertArrayHasKey('price', (array)$latest);
-        $this->assertArrayHasKey('strike_out_price', (array)$latest);
-        $this->assertArrayHasKey('is_active', (array)$latest);
-        $this->assertArrayHasKey('created_at', (array)$latest);
-        $this->assertArrayHasKey('updated_at', (array)$latest);
-        $this->assertArrayHasKey('first_name', (array)$latest);
-        $this->assertArrayHasKey('last_name', (array)$latest);
     }
 }
